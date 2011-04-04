4aed5d9ba80dcb5e8fd4034f8391f710a9ae73e8
Yongseok Kang <feelform@gmail.com>
2011-04-04 21:44:54 +0900

    first commit

---
+++ iCometChat/ChatPort.m
@@ -0,0 +1,14 @@
+//
+//  ChatPort.m
+//  iCometChat
+//
+//  Created by Yongseok Kang on 4/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ChatPort.h"
+
+
+@implementation ChatPort
+
+@end
---
+++ iCometChat/ChatRoom.h
@@ -0,0 +1,16 @@
+//
+//  ChatRoom.h
+//  iCometChat
+//
+//  Created by Yongseok Kang on 4/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ChatRoom : NSObject {
+    
+}
+
+@end
